bbcc0fa72beb9fc7848af49f489b0b2b7ea51636
gimin0226 <138752849+gimin0226@users.noreply.github.com>
2023-07-06 15:41:07 +0900

    Update 모음 사전.java

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.java
@@ -2,6 +2,13 @@ class Solution {
     public int solution(String word) {
         String str = "AEIOU";
         int[] RateOfIncrease = {781,156,31,6,1};
+        //aaaaa -> aaaae 1회
+        //aaaa-> aaae  (aaaa-> (aaaaa->aaaae->aaaai->aaaao->aaaau) ->aaae ) 
+        //길이가 4일 때는 길이가 5에서 총 5번 변화한 후 길이가 4로 돌아옴
+        //따라서 5+1
+        //aaa-> aae 
+        //길이가 3일 때는 길이가 4일때의 증가율 *5+1
+        //따라서 길이가 n일 때는 (길이가 n-1일때의 증가율)*5 + 1 이다.
         int answer=0;
         int index;
         
