c21cf8aea56ee44e80efe0dcb77ae66d91ee84da
gimin0226 <138752849+gimin0226@users.noreply.github.com>
2023-07-07 13:42:16 +0900

    Update 카펫.java

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -1,8 +1,6 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-
         
-       
         for(int m=0;m<10000;m++){   
             for(int n=m;n<10000;n++){        
                 if(n>=m&&2*n+2*m+4==brown&&n*m==yellow){
