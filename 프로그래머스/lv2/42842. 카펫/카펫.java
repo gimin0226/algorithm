079015760743c8f9f3e475456154bdc11073ab36
gimin0226 <138752849+gimin0226@users.noreply.github.com>
2023-07-07 13:37:32 +0900

    [level 2] Title: 카펫, Time: 1.17 ms, Memory: 78.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+
+        
+       
+        for(int m=0;m<10000;m++){   
+            for(int n=m;n<10000;n++){        
+                if(n>=m&&2*n+2*m+4==brown&&n*m==yellow){
+                    return new int[]{n+2,m+2};
+                }
+            }
+        }
+        return null;
+    }
+}
