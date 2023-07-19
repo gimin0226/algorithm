a6ddcd6e47bc1db94282fab2ee4e7b4e6204c4f1
gimin0226 <138752849+gimin0226@users.noreply.github.com>
2023-07-19 16:37:06 +0900

    [level 3] Title: 정수 삼각형, Time: 5.58 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,15 +1,10 @@
 class Solution {
-
     public int solution(int[][] triangle) {
-        int n=triangle.length-1;
-        for(int j=n;j>0;j--){
+        for(int j=triangle.length-1;j>0;j--){
             for(int i=0;i<j;i++){
                 triangle[j-1][i]+=(triangle[j][i]>triangle[j][i+1])?triangle[j][i]:triangle[j][i+1];
             }
         }
-        
-        int answer=triangle[0][0];
-        return answer;
+        return triangle[0][0];   
     }
-
 }
