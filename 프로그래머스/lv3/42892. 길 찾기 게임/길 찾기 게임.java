3ca4b89140c26ed58a70eb27a98b764b37e4275b
gimin0226 <138752849+gimin0226@users.noreply.github.com>
2023-07-30 17:07:42 +0900

    [level 3] Title: 길 찾기 게임, Time: 2.83 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/lv3/42892. 길 찾기 게임/길 찾기 게임.java
+++ 프로그래머스/lv3/42892. 길 찾기 게임/길 찾기 게임.java
@@ -1,74 +1,10 @@
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Comparator;
+
 class Solution {
-    class Node{
-        public Node left;
-        public Node right;
-        
-        public final int data;
-        public final int x;
-        public final int y;
-        
-        public Node(int data,int x,int y){
-            this.data = data;
-            this.x = x;
-            this.y = y;
-        }
-    }
-    
-    public void LinkedNode(Node move,Node node){ 
-        while(true){
-            if(node.x>move.x){
-                if(move.right==null){
-                    move.right=node;
-                    break;
-                }else{
-                    move=move.right;
-                }
-            }else if(node.x<move.x){
-                if(move.left==null){
-                    move.left=node;
-                    break;
-                }else{
-                    move=move.left;
-                }
-            }
-        }
-    }
-    
-    
-    public Node MakeTree(Node[] nodes){
-        Node root= nodes[0];
-        for(int i=1;i<nodes.length;i++){
-            LinkedNode(root,nodes[i]);
-        }
-        return root;
-    }
-    
-    
-    public void preorder(Node node,List<Integer> list){
-        if(node==null) return;
-        
-        list.add(node.data);
-        preorder(node.left,list);
-        preorder(node.right,list);
-    }
-    
-    
-    public void postorder(Node node,List<Integer> list){
-        if(node==null){
-           return;
-        }
-        
-        postorder(node.left, list);
-        postorder(node.right, list);
-        list.add(node.data);
-      //  System.out.println(node.data+" ");
-    }
-    
-    
     public int[][] solution(int[][] arr) {
-        /*
+        
         Arrays.sort(arr,new Comparator<int[]>() {
 				public int compare(int[] o1,int[] o2) {
 					if(o1[1]==o2[1]) {
@@ -85,30 +21,7 @@ class Solution {
 				}
 				System.out.println();
         }
-        
-        Node[] nodes= new Node[arr.length];
-        for(int i=0;i<nodes.length;i++){
-            nodes[i]=new Node(i+1,arr[i][0],arr[i][1]);
-        }*/
-        
-        Node[] nodes= new Node[arr.length];
-        for(int i=0;i<nodes.length;i++){
-            nodes[i] = new Node(i+1,arr[i][0],arr[i][1]);
-        }
-        Arrays.sort(nodes, (a,b)->b.y-a.y);
-        
-        Node root=MakeTree(nodes);
-       // System.out.println(root.right.right.data);
-        
-        List<Integer> preorderList = new ArrayList<Integer>();
-        preorder(root,preorderList);
-        
-        List<Integer> postorderList=new ArrayList<Integer>();
-        postorder(root,postorderList);
-        
-        return new int[][]{
-            preorderList.stream().mapToInt(Integer::intValue).toArray(),
-            postorderList.stream().mapToInt(Integer::intValue).toArray(),
-        };
+        int[][] answer = {};
+        return answer;
     }
 }
