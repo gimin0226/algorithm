a93d280a19246aa75a6edb12a2941b30975d2f2e
gimin0226 <138752849+gimin0226@users.noreply.github.com>
2023-08-27 13:12:27 +0900

    [Gold III] Title: 사다리 조작, Time: 1596 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.cc
@@ -0,0 +1,130 @@
+#include<iostream>	
+#include<cstdio>
+using namespace std;
+
+int n, m, h;
+int** arr;
+int** road;
+int minn = 100;
+void BackTracking(int num) {
+
+	if (num > 3) {
+		return;
+	}
+
+	for (int i = 1; i <= h; i++) {
+		for (int j = 1; j < n; j++) {
+			if (road[i][j] == 0&&road[i][j-1]!=1&&road[i][j+1]!=1) {
+				road[i][j] = 1;
+				int count = 0;
+				int x, y;
+				for (int t = 1; t <= n; t++) {
+					x = 1; y = t;
+
+					int k = 0;
+					for (k = x; k <= h; k++) {
+						if (road[k][y]==1) {
+							y += 1;
+						//	printf("x:%d y:%d\n", k, y);
+						}
+						else if (road[k][y-1]==1) {
+							y -= 1;
+						//	printf("x:%d y:%d\n", k, y);
+						}
+					}
+
+					if (y == t) {
+						count += 1;
+					}
+					else {
+						//printf("i:%d j:%d y:%d t:%d", i, j, y, t);
+						break;
+					}			
+				}
+				if (count == n) {
+					if (num < minn) {
+						minn = num;
+					}
+				}
+				road[i][j] = 0;
+			}
+		}
+	}
+
+	for (int i = 1; i <= h; i++) {
+		for (int j = 1; j <n; j++) {
+			if (road[i][j] == 0 && road[i][j - 1] != 1 && road[i][j + 1] != 1) {
+				road[i][j] = 1;
+				BackTracking(num + 1);
+				road[i][j] = 0;
+			}
+		}
+	}
+}
+int main(void) {
+	cin >> n >> m >> h;
+	road = new int* [h+2];
+	for (int i = 1; i <= h; i++) {
+		
+		road[i] = new int[n +1];
+		for (int j = 1; j < n; j++) {
+			road[i][j] = 0;
+		}
+	}
+	for (int i = 1; i <= h; i++) {
+		road[i][0] = -1;
+		road[i][n] = -1;
+	}
+	
+	for (int i = 0; i < m; i++) {
+		int x, y;
+		cin >> x >> y;
+		road[x][y] = 1;
+	}
+
+	/*for (int i = 1; i <= h; i++) {
+		for (int j = 0; j <=n; j++) {
+			cout << road[i][j] << " ";
+		}cout << endl;
+	}
+	*/
+	int count = 0;
+	int x, y;
+	for (int t = 1; t <= n; t++) {
+		x = 1; y = t;
+
+		int k = 0;
+		for (k = x; k <= h; k++) {
+			if (road[k][y] == 1) {
+				y += 1;
+				//	printf("x:%d y:%d\n", k, y);
+			}
+			else if (road[k][y - 1] == 1) {
+				y -= 1;
+				//	printf("x:%d y:%d\n", k, y);
+			}
+		}
+
+		if (y == t) {
+			count += 1;
+		}
+		else {
+			//printf("i:%d j:%d y:%d t:%d", i, j, y, t);
+			break;
+		}
+	}
+	if (count == n) {
+	/*	for (int i = 1; i <= h; i++) {
+			for (int j = 0; j <= n; j++) {
+				cout << road[i][j] << " ";
+			}cout << endl;
+		}*/
+		cout << "0";
+		return 0;
+	}
+	BackTracking(1);
+	if (minn == 100)
+		cout << "-1";
+	else
+		cout << minn;
+}
