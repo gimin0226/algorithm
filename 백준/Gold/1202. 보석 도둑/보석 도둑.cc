7925f1dcc1e6e92d1744e266b0bd9f932b5f88b2
gimin0226 <138752849+gimin0226@users.noreply.github.com>
2023-08-28 00:34:46 +0900

    [Gold II] Title: 보석 도둑, Time: 648 ms, Memory: 20120 KB -BaekjoonHub

---
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.cc
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<algorithm>
+#include<set>
+using namespace std;
+
+
+int main(void) {
+	int n, m;
+	cin >> n >> m;
+	priority_queue<pair<int, int>> pq;
+	multiset<int> myset;
+
+	for (int i = 0; i < n; i++) {
+		int x, y;
+		cin >> x >> y;
+		pq.push({ y,x });
+	}
+	for (int i = 0; i < m; i++) {
+		int x;
+		cin >> x;
+		myset.insert(x);
+	}
+
+	long long sum = 0;
+	int num = m;
+	while (!pq.empty()) {
+		if (num == 0) {
+			break;
+		}
+		int cost=pq.top().first;
+		int weight=pq.top().second;
+		pq.pop();
+		auto it = myset.lower_bound(weight);
+		
+		if (it != myset.end()) {
+			int t = *it;
+			myset.erase(it);
+			sum += cost;
+			//cout << t<<" "<<cost << " " << weight << " " << sum << "\n";
+			num -= 1;
+		}
+	}
+	cout << sum;
+}
