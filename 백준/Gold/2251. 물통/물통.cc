c32383580b0ab0770b8c3b05cf24b249a3840b6e
gimin0226 <138752849+gimin0226@users.noreply.github.com>
2023-09-08 14:46:24 +0900

    [Gold V] Title: 물통, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Gold/2251. 물통/물통.cc
+++ 백준/Gold/2251. 물통/물통.cc
@@ -3,32 +3,33 @@
 using namespace std;
 vector<int> vec;
 struct tuple_hash {
-	template <class T1, class T2, class T3>
-	std::size_t operator() (const std::tuple<T1, T2, T3>& t) const {
-		auto h1 = std::hash<T1>{}(std::get<0>(t));
-		auto h2 = std::hash<T2>{}(std::get<1>(t));
-		auto h3 = std::hash<T3>{}(std::get<2>(t));
-		return h1 ^ h2 ^ h3;
-	}
+    template <class T1, class T2, class T3>
+    std::size_t operator() (const std::tuple<T1, T2, T3>& t) const {
+        auto h1 = std::hash<T1>{}(std::get<0>(t));
+        auto h2 = std::hash<T2>{}(std::get<1>(t));
+        auto h3 = std::hash<T3>{}(std::get<2>(t));
+        return h1 ^ h2 ^ h3;
+    }
 };
 
 unordered_map<tuple<int, int, int>, bool, tuple_hash> um;
 int siz[3];
 
 void recursion(int a, int b, int c) {
-    if (um.find({a, b, c}) != um.end()) return;
+    if (um.find({ a, b, c }) != um.end()) return;
     if (a == 0) vec.push_back(c);
     um[{a, b, c}] = true;
 
-    int water[]{a, b, c};
+    int water[]{ a, b, c };
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             if (i == j) continue;
-            int arr[3] = {a, b, c};
+            int arr[3] = { a, b, c };
             if (water[i] + water[j] <= siz[j]) {
                 arr[j] += arr[i];
                 arr[i] = 0;
-            } else {
+            }
+            else {
                 arr[i] -= (siz[j] - arr[j]);
                 arr[j] = siz[j];
             }
