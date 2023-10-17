55912cd30436387e2268d071ca791869114550a2
gimin0226 <138752849+gimin0226@users.noreply.github.com>
2023-10-18 00:30:21 +0900

    [Gold IV] Title: 배열 돌리기 4, Time: 12 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.cc
+++ 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.cc
@@ -11,9 +11,7 @@ void turn(int x, int y, int z) {
     int x_s = x - z - 1;
     int y_s = y - z - 1;
     int x_e = x + z - 1;
-    int y_e = y + z - 1;
-  //  printf("x_s:%d y_s:%d x_e:%d y_e:%d\n", x_s, y_s, x_e, y_e);
-   
+    int y_e = y + z - 1; 
     while (true) {
         if (x_s == x_e || y_s == y_e)break;
         if (x_e - x_s == 1 ) {
@@ -43,8 +41,7 @@ void turn(int x, int y, int z) {
         int v = arr[x_s][y_s];
         int di= 0;
         int a = x_s, b = y_s;
-        while (true) {
-          //  cout << "a:" << a << " b:" << b << endl;
+        while (true) {       
             if (a + dx[di] == x_s && b+ dy[di] == y_s) {
                 a += dx[di]; b += dy[di];
                 swap(v, arr[a][b]);
@@ -59,20 +56,7 @@ void turn(int x, int y, int z) {
 
         }
         x_s += 1; x_e -= 1; y_s += 1; y_e -= 1;
-    //    cout << endl;
-       /* for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                cout << arr[i][j] << " ";
-            }cout << endl;
-        }cout << endl;*/
-    }/*
-    cout << endl;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cout << arr[i][j] << " ";
-        }cout << endl;
-    }cout << endl;
-    */
+    }
 }
 
 void f(int ti) {
@@ -83,14 +67,12 @@ void f(int ti) {
             for (int j = 0; j < m; j++) {
                 sum += arr[i][j];
             }
-         //   cout << sum << endl;
             minn = min(sum, minn);
         }
         return;
     }
     for (int i = 0; i < vec.size(); i++) {
         if (check[i] == 1)continue;
-
         int x, y, z;  tie(x, y, z) = vec[i];
         int arr2[51][51];
         for (int t = 0; t < n; t++) {
@@ -114,20 +96,17 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-  
     cin >> n >> m >> k;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             cin >> arr[i][j];
         }
     }
-
     for (int i = 0; i < k; i++) {
         int x, y, z;
         cin >> x >> y >> z;
         vec.push_back({ x,y,z });
-    }
-    
+    }    
     f(0);
     cout << minn<<'\n';
     return 0;
