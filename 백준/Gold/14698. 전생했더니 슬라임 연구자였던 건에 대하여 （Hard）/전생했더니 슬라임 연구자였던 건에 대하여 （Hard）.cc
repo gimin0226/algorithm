3eeb530c9eca56c728f5a9013681337b23317ef4
gimin0226 <138752849+gimin0226@users.noreply.github.com>
2023-10-17 20:04:08 +0900

    [Gold IV] Title: 전생했더니 슬라임 연구자였던 건에 대하여 (Hard), Time: 188 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/14698. 전생했더니 슬라임 연구자였던 건에 대하여 （Hard）/전생했더니 슬라임 연구자였던 건에 대하여 （Hard）.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+   
+    int t;
+    cin >> t;
+    for (int i = 0; i < t; i++) {
+        priority_queue<long long, vector<long long>, greater<>> pq;
+        int n;
+        cin >> n;
+        
+        long long t;
+        for (int i = 0; i < n; i++) {
+            cin >> t;
+            pq.push(t);
+        }
+
+        if (n == 1) {
+            cout << "1"<<'\n';
+            continue;
+        }
+        long long sum = 1;
+        while (true) {
+            long long x = pq.top(); pq.pop();
+            long long y = pq.top(); pq.pop();
+            long long t = (x *y );
+           // sum = sum * t;
+          //  cout << t << endl;
+            sum = ((sum % 1000000007) * (t % 1000000007)) % 1000000007;
+         //   cout << t << endl;
+            if (pq.empty()) {
+                cout << sum  << '\n';
+                break;
+            }
+            pq.push(t);
+        }
+    }
+    return 0;
+}
